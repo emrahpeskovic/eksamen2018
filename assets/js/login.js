f409c90e0556840bc2666e77cea99778b684170f
Solveig Hagensen <solv0678@edu.eal.dk>
2018-05-21 15:58:15 +0200

    image_slider

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,20 +1,19 @@
 var attempt = 3;
 
-function LogMeIn(){
-var username = document.getElementById("UserId").value;
-var password = document.getElementById("Password").value;
-if ( username == "admin" && password == "1234"){
-alert ("You are now logged in!");
-window.location = "index.html";
-return false;
-}
-else{
-attempt --;
-alert("You have left "+attempt+" attempt;");
-if( attempt == 0){
-document.getElementById("UserId").disabled = true;
-document.getElementById("Password").disabled = true;
-document.getElementById("Submit").disabled = true;
-return false;
-}
-}
+function LogMeIn() {
+  var username = document.getElementById("UserId").value;
+  var password = document.getElementById("Password").value;
+  if (username == "admin" && password == "1234") {
+    alert("You are now logged in!");
+    window.location = "index.html";
+    return false;
+  } else {
+    attempt--;
+    alert("You have left " + attempt + " attempt;");
+    if (attempt == 0) {
+      document.getElementById("UserId").disabled = true;
+      document.getElementById("Password").disabled = true;
+      document.getElementById("Submit").disabled = true;
+      return false;
+    }
+  }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 function initMap() {
-    var varde = {
-        lat: 55.623335,
-        lng: 8.481852
-    };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: varde
-    });
-    var marker = new google.maps.Marker({
-        position: varde,
-        map: map
-    });
+  var varde = {
+    lat: 55.623335,
+    lng: 8.481852
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: varde
+  });
+  var marker = new google.maps.Marker({
+    position: varde,
+    map: map
+  });
 }
 
 /* Program Javascript */
@@ -21,29 +21,29 @@ var acc = document.getElementsByClassName("day");
 var panel = document.getElementsByClassName('panel');
 // Denne kører for hver gang der er et element der har classen "day"
 for (var i = 0; i < acc.length; i++) {
-    // Dette er en funktion når man klikker på et element der har classen "day"
-    acc[i].onclick = function() {
-      // Denne variabel tjekker om den du klikker har på classen "active"
-      var setClasses = !this.classList.contains('active');
-      // Her fjerner man classen active og show på "day" og "panel"
-        setClass(acc, 'active', 'remove');
-        setClass(panel, 'show', 'remove');
-        // Denne tjekker om variabelen setClasses er sand
-        if (setClasses) {
-          // Det element du trykker på får classen "active"
-            this.classList.toggle("active");
-          // Dette tager element lige efter classen "day" som er "panel" og tilføjer classen "show"
-            this.nextElementSibling.classList.toggle("show");
-        }
+  // Dette er en funktion når man klikker på et element der har classen "day"
+  acc[i].onclick = function() {
+    // Denne variabel tjekker om den du klikker har på classen "active"
+    var setClasses = !this.classList.contains('active');
+    // Her fjerner man classen active og show på "day" og "panel"
+    setClass(acc, 'active', 'remove');
+    setClass(panel, 'show', 'remove');
+    // Denne tjekker om variabelen setClasses er sand
+    if (setClasses) {
+      // Det element du trykker på får classen "active"
+      this.classList.toggle("active");
+      // Dette tager element lige efter classen "day" som er "panel" og tilføjer classen "show"
+      this.nextElementSibling.classList.toggle("show");
     }
+  }
 }
 
 // Opretter en funktioner som placerer de forskellige classes
 function setClass(els, className, fnName) {
   // Denne kører hver gang den skal tilføje en class så 2 gange i disse tilfælde
-    for (var i = 0; i < els.length; i++) {
-        els[i].classList[fnName](className);
-    }
+  for (var i = 0; i < els.length; i++) {
+    els[i].classList[fnName](className);
+  }
 }
 
 /* Javascript til datoen */
@@ -53,17 +53,36 @@ var countDownDate = new Date("August 6, 2018").getTime();
 // Denne variabel kører funktionen hvert sekund for at tjekke om der er sket ændringer
 var x = setInterval(function() {
   // Skaffer den nuværende dato
-    var now = new Date().getTime();
-    // Tjekker hvor stor forskel der er på festivals dato og den nuværende dato
-    var distance = countDownDate - now;
-    // Udregner hvor mange dage der er til festivalen
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    // Udregner hvor mange timer der er til eventet
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    // Udregner hvor mange minutter der er til eventet
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    // Smider de forskellige variabler ind i en div med id countdown og smider et <p> tag rundt om
-    document.getElementById("countdown").innerHTML = "<p>" + days + " dage " + hours + " timer " +
-        minutes + " minutter </p>";
+  var now = new Date().getTime();
+  // Tjekker hvor stor forskel der er på festivals dato og den nuværende dato
+  var distance = countDownDate - now;
+  // Udregner hvor mange dage der er til festivalen
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  // Udregner hvor mange timer der er til eventet
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  // Udregner hvor mange minutter der er til eventet
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  // Smider de forskellige variabler ind i en div med id countdown og smider et <p> tag rundt om
+  document.getElementById("countdown").innerHTML = "<p>" + days + " dage " + hours + " timer " +
+    minutes + " minutter </p>";
 }, 1000);
- 
+var listEl = document.querySelector('.carousel');
+ var btnLeftEl = document.querySelector('#left-btn');
+ var btnRightEl = document.querySelector('#right-btn');
+ var count = 0;
+
+ function slideImages(dir){
+   var totalChildren = listEl.querySelectorAll(".item").length;
+   dir === "left" ? ++count : --count;
+   listEl.style.left = count * 286 + 'px';
+   btnLeftEl.style.display = count < 0 ? "block" : "none";
+   btnRightEl.style.display = count > 4-totalChildren ? "block" :
+"none";
+ }
+
+ btnLeftEl.addEventListener("click", function(e) {
+     slideImages("left");
+ });
+ btnRightEl.addEventListener("click", function(e) {
+     slideImages("right");
+ });
