1b6baaa5613992b6618b83ae2668415468348823
Solveig Hagensen <solv0678@edu.eal.dk>
2018-05-21 19:20:54 +0200

    oplev

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -1,4 +1,4 @@
-function ESC() {
+function VM () {
         var elmnt = document.getElementById("oplev");
         elmnt.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
     }
