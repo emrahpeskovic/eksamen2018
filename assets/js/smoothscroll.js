f9ea9f2120829f5669e02e17f2fb76792bfe47cc
Solveig Hagensen <solv0678@edu.eal.dk>
2018-05-22 09:58:04 +0200

    overskrift + smooth scroll

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -1,4 +1,28 @@
-function VM () {
-        var elmnt = document.getElementById("oplev");
+function oplevVM () {
+        var elmnt = document.getElementById("overskrift_oplev");
         elmnt.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
     }
+    function programVM () {
+            var elmnt = document.getElementById("overskrift_program");
+            elmnt.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+        }
+
+        function forinstitutionerVM () {
+                var elmnt = document.getElementById("overskrift_forinstitutioner");
+                elmnt.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+            }
+
+            function blivfrivilligVM () {
+                    var elmnt = document.getElementById("overskrift_blivfrivillig");
+                    elmnt.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+                }
+
+                function findosVM () {
+                        var elmnt = document.getElementById("overskrift_findos");
+                        elmnt.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+                    }
+
+                    function kontaktVM () {
+                            var elmnt = document.getElementById("overskrift_program");
+                            elmnt.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+                        }
